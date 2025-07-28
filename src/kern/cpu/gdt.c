bf65d3077bdf45a949a0c3d96fdc2a5cfbd621b8
kthread0 <dev@kthread.dev>
2025-07-28 19:16:18 -0300

    im gonna lose it

--- src/kern/cpu/gdt.c
+++ src/kern/cpu/gdt.c
@@ -9,7 +9,7 @@ typedef struct {
 	uint64_t kernel_data; // 0x00CF92000000FFFF Kernel DATA
 	uint64_t user_code;		// 0x00CFFA000000FFFF User CODE
 	uint64_t user_data;		// 0x00CFF2000000FFFF User DATA
-												// uint64_t tss;					// 0x00FFE9000000FFFF Task Stack Segment
+	uint64_t tss;					// 0x00FFE9000000FFFF Task Stack Segment
 } __attribute__((packed)) gdt_t;
 
 typedef struct {
@@ -19,21 +19,20 @@ typedef struct {
 
 gdt_t gdt;
 
+gdt_descriptor_t descriptor = {
+	.limit = sizeof(gdt) - 1,
+	.base = (uint64_t) &gdt,
+};
+
 void gdt_init(void) {
 	gdt.null = 0x0000000000000000;
 	gdt.kernel_code = 0x00CF92000000FFFF;
 	gdt.kernel_data = 0x00CF9A000000FFFF;
 	gdt.user_code = 0x00CFF2000000FFFF;
 	gdt.user_data = 0x00CFF2000000FFFF;
-	// gdt.tss = 0x00FFE9000000FFFF;
-
-	gdt_descriptor_t descriptor = {
-		.limit = sizeof(gdt) - 1,
-		.base = (uint64_t) &gdt,
-	};
+	gdt.tss = 0x00FFE9000000FFFF;
 
 	__asm__ volatile("lgdt %0" : : "m"(descriptor));
-	reload_segments();
 
 	serial_printf("garmin, initialize gdt!\n");
 }
--- src/kern/cpu/gdt.h
+++ src/kern/cpu/gdt.h
@@ -1,4 +1,3 @@
 #include <stdint.h>
 
 void gdt_init(void);
-extern void reload_segments();
