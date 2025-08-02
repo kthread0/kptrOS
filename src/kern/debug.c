28bc8f874a1d0cc94669349dcf07eea6a89ad72e
kthread0 <dev@kthread.dev>
2025-08-02 20:16:58 -0300

    fix formatting

--- src/kern/debug.c
+++ src/kern/debug.c
@@ -5,25 +5,29 @@
 #include <system.h>
 
 // Entry point request
-static volatile struct limine_executable_address_request executable_address_request
-								__attribute__((aligned(8))) = { .id = LIMINE_EXECUTABLE_ADDRESS_REQUEST, .revision = 0 };
+static volatile struct limine_executable_address_request
+		executable_address_request __attribute__((aligned(8))) = {
+				.id = LIMINE_EXECUTABLE_ADDRESS_REQUEST, .revision = 0};
 
 // SMBIOS request
 static volatile struct limine_smbios_request smbios_request
-								__attribute__((aligned(8))) = { .id = LIMINE_SMBIOS_REQUEST, .revision = 0 };
+		__attribute__((aligned(8))) = {.id = LIMINE_SMBIOS_REQUEST, .revision = 0};
 
 // Paging mode request for 4-level paging
-static volatile struct limine_paging_mode_request paging_mode_request = { .id = LIMINE_PAGING_MODE_REQUEST,
-																																					.revision = 0,
-																																					.mode = LIMINE_PAGING_MODE_X86_64_4LVL,
-																																					.max_mode = LIMINE_PAGING_MODE_X86_64_4LVL,
-																																					.min_mode = LIMINE_PAGING_MODE_X86_64_4LVL };
+static volatile struct limine_paging_mode_request paging_mode_request = {
+		.id = LIMINE_PAGING_MODE_REQUEST,
+		.revision = 0,
+		.mode = LIMINE_PAGING_MODE_X86_64_4LVL,
+		.max_mode = LIMINE_PAGING_MODE_X86_64_4LVL,
+		.min_mode = LIMINE_PAGING_MODE_X86_64_4LVL};
 
 // Firmware type request
-static volatile struct limine_firmware_type_request firmware_type_request = { .id = LIMINE_FIRMWARE_TYPE_REQUEST, .revision = 0 };
+static volatile struct limine_firmware_type_request firmware_type_request = {
+		.id = LIMINE_FIRMWARE_TYPE_REQUEST, .revision = 0};
 
 // Stack size request
-static volatile struct limine_stack_size_request stack_size_request = { .id = LIMINE_STACK_SIZE_REQUEST, .revision = 0 };
+static volatile struct limine_stack_size_request stack_size_request = {
+		.id = LIMINE_STACK_SIZE_REQUEST, .revision = 0};
 
 void debug_limine_requests() {
 	// Kernel address request
@@ -33,7 +37,8 @@ void debug_limine_requests() {
 		capture_cpu_state(&state);
 		panic(&state);
 	} else {
-		serial_printf("Kernel address: %x\n", executable_address_request.response->virtual_base);
+		serial_printf("Kernel address: %x\n",
+									executable_address_request.response->virtual_base);
 	}
 
 	// SMBIOS request
@@ -62,7 +67,8 @@ void debug_limine_requests() {
 		capture_cpu_state(&state);
 		panic(&state);
 	} else {
-		serial_printf("Firmware type: %d\n", firmware_type_request.response->firmware_type);
+		serial_printf("Firmware type: %d\n",
+									firmware_type_request.response->firmware_type);
 	}
 
 	if (stack_size_request.response == NULL) {
